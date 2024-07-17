cece656403ce2fe51306c6b1abb848694e01436a
Daniyil Preen <daniyil.aurora@gmail.com>
2024-07-17 13:39:20 -0500

    second commit

---
+++ src/main/java/net/daniyil/adminitems/AdminItems.java
@@ -0,0 +1,17 @@
+package net.daniyil.adminitems;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdminItems extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
